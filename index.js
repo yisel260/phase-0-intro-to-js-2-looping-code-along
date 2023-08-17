b449e65d4be5bf2cf2ba8e79822223bfbd7648b8
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-08-17 16:53:42 -0400

    complete loops in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = [ "Ana", "Mary", "Speedy", ]
+const messages = [];
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages 
+  }
+  function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log (countDown)
+        countDown--
+    }
+  } 
